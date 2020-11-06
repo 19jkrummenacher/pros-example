c7e89093e4310bbfb14b13d9ba196d207779aa43
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2020-11-06 01:34:41 -0700

    drive auton functions implemented

--- include/drive.hpp
+++ include/drive.hpp
@@ -6,21 +6,31 @@ class Drive : public tarvis::System
   public:
     static Drive &get();
     void moveY(double distance, double velocity, int timeout = 1000, int endWait = 100);
+    void moveX(double distance, double velocity, int timeout = 1000, int endWait = 100);
+    void moveZ(double distance, double velocity, int timeout = 1000, int endWait = 100);
     bool isSettled(int v);
-    double getXPos();
-    double getYPos();
-    double getZPos();
+    double getXDeg();
+    double getYDeg();
+    double getZDeg();
 
   private:
     Drive();
+    double yTargetDistance{0};
+    double xTargetDistance{0};
+    double zTargetDistance{0};
     const double wheelCir = 12.56;
     MotorGroup front_left_wheel{{1, 2}};
     MotorGroup front_right_wheel{{2, 3}};
     MotorGroup back_left_wheel{{4, 5}};
     MotorGroup back_right_wheel{{6, 7}};
-    tarvis::Trapezoidal rampY{5, 10, 1, 1, [&](double output) { front_left_wheel.moveVelocity(output); }};
-    tarvis::Trapezoidal rampX{5, 10, 1, 1, [&](double output) { front_left_wheel.moveVelocity(output); }};
-    tarvis::Trapezoidal rampZ{5, 10, 1, 1, [&](double output) { front_left_wheel.moveVelocity(output); }};
+    tarvis::Trapezoidal rampY{5, 10, 200, -200, [&](double output) {}};
+    tarvis::Trapezoidal rampX{5, 10, 200, -200, [&](double output) {}};
+    tarvis::Trapezoidal rampZ{5, 10, 200, -200, [&](double output) {
+                                  front_left_wheel.moveVelocity(rampY.output() + rampX.output() + rampZ.output());
+                                  front_right_wheel.moveVelocity(rampY.output() - rampX.output() - rampZ.output());
+                                  back_left_wheel.moveVelocity(rampY.output() - rampX.output() + rampZ.output());
+                                  back_right_wheel.moveVelocity(rampY.output() + rampX.output() - rampZ.output());
+                              }};
 
     void userCallstack() override;
     void userInit() override;
@@ -29,6 +39,10 @@ class Drive : public tarvis::System
     void autonCallstack() override;
     void autonInit() override;
     void autonDeInit() override;
+    void wait(int endWait, double vSettled, int timeout, bool y, bool x, bool z);
+    const double length = 15; // front to back wheels measured from axil to axil
+    const double width =
+        15; // left to right wheels measured from the left of the left wheel to the left of the right wheel
 };
 extern Drive &drive;
 
--- src/drive.cpp
+++ src/drive.cpp
@@ -28,17 +28,30 @@ Drive::Drive()
     back_left_wheel.tarePosition();
     back_right_wheel.tarePosition();
 }
-void Drive::autonCallstack() {}
+void Drive::autonCallstack()
+{
+    //? uncoment these blocks if you want to run all of the drive ramping calcultions in one task
+    // rampX.calculate();
+    // rampY.calculate();
+    // rampZ.calculate();
+
+    // front_left_wheel.moveVelocity(rampY.output() + rampX.output() + rampZ.output());
+    // front_right_wheel.moveVelocity(rampY.output() - rampX.output() - rampZ.output());
+    // back_left_wheel.moveVelocity(rampY.output() - rampX.output() + rampZ.output());
+    // back_right_wheel.moveVelocity(rampY.output() + rampX.output() - rampZ.output());
+}
 void Drive::autonInit()
 {
     userDeInit();
 
+    //? coment if you want to run all of the drive ramping calcultions in one task
     rampX.start();
     rampY.start();
     rampZ.start();
 }
 void Drive::autonDeInit()
 {
+    //? comment if you want to run all of the drive ramping calcultions in one task
     rampX.suspend();
     rampY.suspend();
     rampZ.suspend();
@@ -57,33 +70,140 @@ bool Drive::isSettled(int v)
            (std::abs(front_right_wheel.getActualVelocity()) < v) &&
            (std::abs(back_left_wheel.getActualVelocity()) < v) && (std::abs(back_right_wheel.getActualVelocity()) < v);
 }
-double Drive::getYPos() { return back_left_wheel.getPosition(); }
 
-void Drive::moveY(double distance, double speed, int timeout, int endWait)
-{ // assumes velocity start = end = 0
-    double direction;
-    double targetDeg = distance * 360 / wheelCir;
+double Drive::getYDeg()
+{ // deg
+    return (front_left_wheel.getPosition() + front_right_wheel.getPosition() + back_left_wheel.getPosition() +
+            back_right_wheel.getPosition()) /
+           4;
+}
+double Drive::getXDeg()
+{ // deg
+    return (front_left_wheel.getPosition() - front_right_wheel.getPosition() - back_left_wheel.getPosition() +
+            back_right_wheel.getPosition()) /
+           4;
+}
+double Drive::getZDeg()
+{ // deg
+    return (front_left_wheel.getPosition() - front_right_wheel.getPosition() + back_left_wheel.getPosition() -
+            back_right_wheel.getPosition()) /
+           (4 * 360) * (wheelCir / ((length / 2) + (width / 2)) * (180 / M_PI));
+}
 
-    direction = (targetDeg - getYPos()) > 0 ? 1 : -1;
-    int velocity = std::abs(speed) * direction;
-    rampY.request(velocity);
-    if (direction > 0)
-    {
-        while (back_left_wheel.getPosition() < targetDeg)
-        {
-            pros::delay(motorUpdateRate);
-        }
-    }
-    else
-    {
-        while (back_left_wheel.getPosition() > targetDeg)
-        {
-            pros::delay(motorUpdateRate);
-        }
-    }
-    rampY.request(0);
-    while (isSettled(1))
+void Drive::wait(int endWait, double vSettled, int timeout, bool y, bool x, bool z)
+{ // bool yxz is for what to stop and wait for
+
+    // request drive to stop
+    if (y) rampY.request(0);
+    if (x) rampX.request(0);
+    if (z) rampZ.request(0);
+    while (y ? rampY.output() != 0 : false || x ? rampX.output() != 0 : false || z ? rampZ.output() != 0 : false)
     {
         pros::delay(motorUpdateRate);
+    }
+    // drive has been requested to stop
+    // wait for drive to actualy come to a stop/settle with a timeout incase of error
+    const int tStart = pros::millis();
+    while (!isSettled(vSettled) && pros::millis() < tStart + timeout)
+    { // wait for the actual velocity to be acceptiable with in the time out window
+        pros::delay(motorUpdateRate);
+    }
+    // drive is actualy settled or timeout has happened
+    pros::delay(endWait);
+}
+void Drive::moveY(double distance, double velocity, int timeout, int endWait)
+{
+    double Deg = distance * 360 / wheelCir;
+    yTargetDistance = distance;
+    int Dir = (Deg - getYDeg()) > 1 ? 1 : -1;
+    velocity = abs(velocity) * Dir;
+
+    bool done;
+    while (!done)
+    {
+        if (Dir > 0 ? getYDeg() < Deg
+                    : getYDeg() >
+                          Deg) // todo subract overshot/last triangle to end on distance rather than stop at distance
+        {                      // y not past abs target
+            rampY.request(velocity);
+            done = false;
+        }
+        else
+        {
+            done = true;
+            rampY.request(0);
+            // todo wait in loop for drive to settle
+        }
+    }
+    if (endWait >= 0)
+    { // default; set stop, wait for stop, wait for endwait;
+        wait(endWait, 0, 1000, true, false, false);
+    }
+    else
+    { // stop no wait; junction/running another drive movement dont worry about waiting for the drive to stop
+    }
+}
+void Drive::moveX(double distance, double velocity, int timeout, int endWait)
+{
+    double Deg = distance * 360 / wheelCir;
+    xTargetDistance = distance;
+    int Dir = (Deg - getXDeg()) > 1 ? 1 : -1;
+    velocity = abs(velocity) * Dir;
+
+    bool done;
+    while (!done)
+    {
+        if (Dir > 0 ? getXDeg() < Deg
+                    : getXDeg() >
+                          Deg) // todo subract overshot/last triangle to end on distance rather than stop at distance
+        {                      // y not past abs target
+            rampX.request(velocity);
+            done = false;
+        }
+        else
+        {
+            done = true;
+            rampX.request(0);
+            // todo wait in loop for drive to settle
+        }
+    }
+    if (endWait >= 0)
+    { // default; set stop, wait for stop, wait for endwait;
+        wait(endWait, 0, 1000, false, true, false);
+    }
+    else
+    { // stop no wait; junction/running another drive movement dont worry about waiting for the drive to stop
+    }
+}
+void Drive::moveZ(double distance, double velocity, int timeout, int endWait)
+{
+    double Deg = distance;
+    zTargetDistance = distance;
+    int Dir = (Deg - getZDeg()) > 1 ? 1 : -1;
+    velocity = abs(velocity) * Dir;
+
+    bool done;
+    while (!done)
+    {
+        if (Dir > 0 ? getZDeg() < Deg
+                    : getZDeg() >
+                          Deg) // todo subract overshot/last triangle to end on distance rather than stop at distance
+        {                      // y not past abs target
+            rampZ.request(velocity);
+            done = false;
+        }
+        else
+        {
+            done = true;
+            rampZ.request(0);
+            // todo wait in loop for drive to settle
+        }
+    }
+    if (endWait >= 0)
+    { // default; set stop, wait for stop, wait for endwait;
+        wait(endWait, 0, 1000, false, false, true);
+    }
+    else
+    { // stop no wait; junction/running another drive movement dont worry about waiting for the drive to stop
     }
 }
